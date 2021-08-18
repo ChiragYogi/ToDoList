5f7626d76482af8038f7685c718060862f0a0a82
= <=>
2021-08-18 17:09:04 +0530

    Ui Completed

---
+++ app/src/main/java/com/example/todolistapp/ItemToDoFragment.kt
@@ -0,0 +1,17 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class ItemToDoFragment : Fragment(R.layout.fragment_item_to_do) {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/todolistapp/ToDoDisplatFragmen.kt
@@ -0,0 +1,20 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ToDoDisplatFragmen : Fragment(R.layout.fragment_to_do_displat) {
+
+
+
+
+
+
+
+
+
+}
